96c50420f4a4c11a3f3792a4581d2c16341d7f23
meghana123-k <kothakondameghana3@gmail.com>
2026-02-20 15:42:02 +0530

    refactor orders details and grid code

--- shop-ekart/src/pages/orders/OrderDetailsGrid.jsx
+++ shop-ekart/src/pages/orders/OrderDetailsGrid.jsx
@@ -1,23 +1,29 @@
-import { formatMoney } from "../../utils/money";
 import dayjs from "dayjs";
-export function OrdersDetailsGrid({order}) {
+import { Fragment } from "react";
+export function OrdersDetailsGrid({ order }) {
   return (
-    <div className="order-header">
-      <div className="order-header-left-section">
-        <div className="order-date">
-          <div className="order-header-label">Order Placed:</div>
-          <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-        </div>
-        <div className="order-total">
-          <div className="order-header-label">Total:</div>
-          <div>${formatMoney(order.totalCostCents)}</div>
-        </div>
-      </div>
+    <div className="order-details-grid">
+      {order.products &&
+        order.products.map((orderProduct) => (
+          <Fragment key={orderProduct.product.id}>
+            <div className="product-image-container">
+              <img src={orderProduct.product.image} />
+            </div>
 
-      <div className="order-header-right-section">
-        <div className="order-header-label">Order ID:</div>
-        <div>{order.id}</div>
-      </div>
+            <div className="product-details">
+              <div className="product-name">{orderProduct.product.name}</div>
+
+              <div className="product-delivery-date">
+                Arriving on:{" "}
+                {dayjs(orderProduct.estimatedDeliveryTimeMs).format("MMMM D")}
+              </div>
+
+              <div className="product-quantity">
+                Quantity: {orderProduct.quantity}
+              </div>
+            </div>
+          </Fragment>
+        ))}
     </div>
   );
 }
--- shop-ekart/src/pages/orders/OrdersGrid.jsx
+++ shop-ekart/src/pages/orders/OrdersGrid.jsx
@@ -1,18 +1,14 @@
 import { OrdersHeader } from "./OrdersHeader";
 import { OrdersDetailsGrid } from "./OrderDetailsGrid";
-export function OrdersGrid({orders}) {
+export function OrdersGrid({ orders }) {
   return (
     <div className="orders-grid">
-      {orders &&
-        orders.map((order) => {
-          return (
-            <div key={order.id} className="order-container">
-              {<OrdersHeader order={order}/>}
-
-              {<OrdersDetailsGrid order={order}/>}
-            </div>
-          );
-        })}
+      {orders.map((order) => (
+        <div key={order.id} className="order-container">
+          <OrdersHeader order={order} />
+          <OrdersDetailsGrid order={order} />
+        </div>
+      ))}
     </div>
   );
 }
