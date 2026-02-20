a072cba5fd775cee4b49090014f4f4583c2e3b77
meghana123-k <kothakondameghana3@gmail.com>
2026-02-20 15:45:37 +0530

    refactor orders details grid code

--- shop-ekart/src/pages/orders/OrderDetailsGrid.jsx
+++ shop-ekart/src/pages/orders/OrderDetailsGrid.jsx
@@ -9,18 +9,29 @@ export function OrdersDetailsGrid({ order }) {
             <div className="product-image-container">
               <img src={orderProduct.product.image} />
             </div>
-
             <div className="product-details">
               <div className="product-name">{orderProduct.product.name}</div>
-
               <div className="product-delivery-date">
                 Arriving on:{" "}
                 {dayjs(orderProduct.estimatedDeliveryTimeMs).format("MMMM D")}
               </div>
-
               <div className="product-quantity">
                 Quantity: {orderProduct.quantity}
               </div>
+              <button className="buy-again-button button-primary">
+                <img
+                  className="buy-again-icon"
+                  src="images/icons/buy-again.png"
+                />
+                <span className="buy-again-message"> Add to Cart </span>
+              </button>
+            </div>
+            <div className="product-actions">
+              <a href="/tracking">
+                <button className="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
             </div>
           </Fragment>
         ))}
