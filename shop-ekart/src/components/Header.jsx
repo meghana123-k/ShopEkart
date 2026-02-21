afabb3afca13447117d2514fa65880f870c5e23b
meghana123-k <kothakondameghana3@gmail.com>
2026-02-21 14:57:42 +0530

    Add search implementation, based ont the input navigate to search input

--- shop-ekart/src/components/Header.jsx
+++ shop-ekart/src/components/Header.jsx
@@ -1,34 +1,65 @@
-import { NavLink } from "react-router";
 import "./header.css";
 import { totalQuantiy } from "../utils/quantity";
+import { Link, useNavigate, useSearchParams } from "react-router";
+import { useState } from "react";
+
 export function Header({ cart }) {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  
+  const searchText = searchParams.get("search");
+  const [inputText, setInputText] = useState(searchText || "");
+  const searchInput = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const searchButton = () => {
+    if (!inputText.trim()) return;
+    navigate(`/?search=${inputText}`);
+  };
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      searchButton();
+    }
+  };
   return (
     <div className="header">
       <div className="left-section">
-        <NavLink to="/" className="header-link">
+        <Link to="/" className="header-link">
           <img className="logo" src="/images/logo-white1.png" />
           <img className="mobile-logo" src="images/mobile-logo-white.png" />
-        </NavLink>
+        </Link>
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search"
+          value={inputText}
+          onChange={searchInput}
+          onKeyDown={handleKeyDown}
+        />
 
         <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
+          <img
+            className="search-icon"
+            src="images/icons/search-icon.png"
+            onClick={searchButton}
+          />
         </button>
       </div>
 
       <div className="right-section">
-        <NavLink className="orders-link header-link" to="/orders">
+        <Link className="orders-link header-link" to="/orders">
           <span className="orders-text">Orders</span>
-        </NavLink>
+        </Link>
 
-        <NavLink className="cart-link header-link" to="/checkout">
+        <Link className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
           <div className="cart-quantity">{totalQuantiy(cart)}</div>
           <div className="cart-text">Cart</div>
-        </NavLink>
+        </Link>
       </div>
     </div>
   );
--- shop-ekart/src/pages/home/HomePage.jsx
+++ shop-ekart/src/pages/home/HomePage.jsx
@@ -2,24 +2,30 @@ import axios from "axios";
 import { Header } from "../../components/Header";
 import "./HomePage.css";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
 import { ProductGrid } from "./ProductGrid";
 export function HomePage({ cart, loadCart }) {
   const [products, setProducts] = useState([]);
-
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get("search");
   useEffect(() => {
-    const getHomeData = async() => {
-      const response = await axios.get("http://localhost:3000/api/products"); 
-        setProducts(response.data);
-      };
+    const getHomeData = async () => {
+      const urlPath = search
+        ? `/api/products$search=${search}`
+        : "api/products";
+      const response = await axios.get(urlPath);
+      setProducts(response.data);
+    };
+
     getHomeData();
-  }, []);
+  }, [search]);
 
   return (
     <>
       <link rel="icon" href="/images/home.png" />
       <Header cart={cart} />
       <div className="home-page">
-        <ProductGrid products={products} loadCart={loadCart}/>
+        <ProductGrid products={products} loadCart={loadCart} />
       </div>
     </>
   );
